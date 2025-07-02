676f3e3e36977a38d6e2ecefb8cf5e4b998cdf17
AliMino <ali.kamel.dev@gmail.com>
2025-07-02 03:49:28 +0300

    Query parsed command info

--- src/Command.php
+++ src/Command.php
@@ -8,7 +8,7 @@ namespace CommandParser;
  * @api
  * @final
  * @since 0.1.0
- * @version 1.0.0
+ * @version 1.1.0
  * @package command-parser
  * @author Ali M. Kamel <ali.kamel.dev@gmail.com>
  */
@@ -34,4 +34,73 @@ final class Command {
         public readonly array $operands = [],
         public readonly array $subCommands = [],
     ) {}
+
+    /**
+     * Retrieves the sub-command.
+     * 
+     * @api
+     * @final
+     * @since 1.1.0
+     * @version 1.0.0
+     * 
+     * @return Command|null Returns the sub-command if exists, otherwise null.
+     */
+    public final function getSubCommand(): ?Command {
+
+        return $this->subCommands[0] ?? null;
+    }
+
+    /**
+     * Retrieves an option by its name.
+     * 
+     * @api
+     * @final
+     * @since 1.1.0
+     * @version 1.0.0
+     * 
+     * @param string $name
+     * @return Option|null Returns the option if exists, otherwise null.
+     */
+    public final function getOption(string $name): ?Option {
+
+        foreach ($this->options as $option) {
+            
+            if ($option->name === $name) {
+                
+                return $option;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Retrieves an operand by its index or name.
+     * 
+     * @api
+     * @final
+     * @since 1.1.0
+     * @version 1.0.0
+     * 
+     * @param int|null $index The index of the operand.
+     * @param string|null $name The name of the operand.
+     * @return Operand|null Returns the operand if exists, otherwise null.
+     */
+    public final function getOperand(?int $index = null, ?string $name = null): ?Operand {
+
+        if (is_null($index ?? $name)) {
+
+            return null;
+        }
+
+        foreach ($this->operands as $operand) {
+            
+            if ((!is_null($index) && $operand->index === $index) || (!is_null($name) && $operand->name === $name)) {
+                
+                return $operand;
+            }
+        }
+
+        return null;
+    }
 }
